f02c4f2f21911514d089b3c7334775983e1948ae
Rob Clarke <robclarke93@yahoo.co.uk>
2021-05-25 20:51:34 -0700

    started working on caesar

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,5 +1,15 @@
-const caesar = function() {
-    //some text
+const caesar = function(text, shift) {
+    for (let i = 0; i < (text.length - 1);i++)
+        if (isAlpha(text[i]) == true) {
+            //if capital
+            if (text.charCodeAt(i) >= 65 && text.charCodeAt(i) <= 90) {
+
+                
+            }
+            //if lowercase
+        } else {
+            //do nothing and move to the next iteration.
+        }
 };
 
 module.exports = caesar;
