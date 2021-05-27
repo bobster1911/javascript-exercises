224371d34deff91748e6abf6b3bd7353d706c6d3
RobMME <robclarke93@yahoo.co.uk>
2021-05-27 14:48:06 -0700

    worked on iterations - caesar

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,14 +1,27 @@
 const caesar = function(text, shift) {
+    let newString = "";
+
     for (let i = 0; i < (text.length - 1);i++)
+
+        let number = text.charCodeAt(i);
         if (isAlpha(text[i]) == true) {
             //if capital
-            if (text.charCodeAt(i) >= 65 && text.charCodeAt(i) <= 90) {
-
-                
+            if (number >= 65 && number <= 90) {
+                //get shifted char code
+                if (shift > (90 - number)) {
+                    newString[i] = 65 + (shift - (90 - number));
+                } else {
+                    newString[i] = 
+                }
             }
-            //if lowercase
+        }
+       }   //if lowercase
+            if (number >= 97 && number <= 122) {
+
+            }
         } else {
             //do nothing and move to the next iteration.
+            continue;
         }
 };
 
