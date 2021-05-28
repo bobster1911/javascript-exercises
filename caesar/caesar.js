c0ec2077ca04cb3cda0cce59210235a24b1275a4
Rob Clarke <robclarke93@yahoo.co.uk>
2021-05-28 07:56:09 -0700

    worked more on caesar and installed jest

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,28 +1,33 @@
 const caesar = function(text, shift) {
     let newString = "";
 
-    for (let i = 0; i < (text.length - 1);i++)
+    for (let i = 0; i < (text.length - 1);i++) {
 
-        let number = text.charCodeAt(i);
         if (isAlpha(text[i]) == true) {
+            let number = text.charCodeAt(i);
             //if capital
             if (number >= 65 && number <= 90) {
                 //get shifted char code
                 if (shift > (90 - number)) {
-                    newString[i] = 65 + (shift - (90 - number));
+                    newString[i] = fromCharCode(65 + (shift - (90 - number)));
                 } else {
-                    newString[i] = 
-                }
-            }
-        }
-       }   //if lowercase
-            if (number >= 97 && number <= 122) {
-
-            }
+                    newString[i] = fromCharCode(number + shift);
+                } // end
+            //if lowercase    
+            } else if (number >= 97 && number <= 122) {
+                if (shift > (122 - number)) {
+                    newString[i] = fromCharCode(97 + (shift - (122 - number)));
+                } else {
+                    newString[i] = fromCharCode(number + shift);
+                } // end
+            } else {
+                console.log("Something has gone wrong.");
+            } // end
         } else {
-            //do nothing and move to the next iteration.
-            continue;
-        }
-};
+                newString[i] = text[i];
+        } // end
+    } // end 
+
+    return newString;
 
 module.exports = caesar;
