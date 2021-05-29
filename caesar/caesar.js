3329be90904c3afd7ce9a529d9ac8109cce6be22
Rob Clarke <robclarke93@yahoo.co.uk>
2021-05-29 12:35:55 -0700

    final draft - caesar

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,34 +1,42 @@
 const caesar = function(text, shift) {
-    let newString = "";
+    let text = prompt("Please enter text: ");
+    let shift = prompt("Please enter shift: ") % 26;
+    
+    let newString = [];
 
-    for (let i = 0; i < (text.length - 1);i++) {
+    for (let i = 0; i < (text.length);i++) {
+        
+        let number = text.charCodeAt(i);
 
-        if (isAlpha(text[i]) == true) {
-            let number = text.charCodeAt(i);
-            //if capital
-            if (number >= 65 && number <= 90) {
-                //get shifted char code
-                if (shift > (90 - number)) {
-                    newString[i] = fromCharCode(65 + (shift - (90 - number)));
-                } else {
-                    newString[i] = fromCharCode(number + shift);
-                } // end
-            //if lowercase    
-            } else if (number >= 97 && number <= 122) {
-                if (shift > (122 - number)) {
-                    newString[i] = fromCharCode(97 + (shift - (122 - number)));
-                } else {
-                    newString[i] = fromCharCode(number + shift);
-                } // end
+        //if capital
+        if (number >= 65 && number <= 90) {
+            //get shifted char code
+            if (shift > (90 - number)) {
+                newString[i] = String.fromCharCode(65 + (shift - (90 - number)));
             } else {
-                console.log("Something has gone wrong.");
+                newString[i] = String.fromCharCode(number + shift);
+            } // end
+        //if lowercase    
+        } else if (number >= 97 && number <= 122) {
+            if (shift > (122 - number)) {
+                newString[i] = String.fromCharCode(97 + (shift - (122 - number)));
+            } else {
+                newString[i] = String.fromCharCode(number + shift);
             } // end
         } else {
-                newString[i] = text[i];
+            newString[i] = text[i];
         } // end
-        
-    } // end 
-    return newString; 
+
+        } // end
+    
+    let output = "";
+    for (i = 0; i < newString.length; i++) {
+         output += newString[i];
+    }
+    return output;
+    
 }
+
+caesar(text, shift);
 
 module.exports = caesar;
