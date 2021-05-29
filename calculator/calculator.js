f4bb6b2c9bc8e9b70c65bf2e61b09f4f08c7d528
Rob Clarke <robclarke93@yahoo.co.uk>
2021-05-29 12:29:12 -0700

    initial draft - caesar, also started calculator

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sum = 0;
+	for (i = 0; i < length.array; i++) {
+    sum += array[i];
+  }
+  return array;
 };
 
-const multiply = function() {
-
+const multiply = function(x, y) {
+  return x * y;
 };
 
-const power = function() {
-	
+const power = function(x, power) {
+	for (i = 0; i < power; i++) {
+    x *= i;
+  }
+  return x;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  let result = factorial;
+	for (i = factorial-1; i > 0; i--) {
+    result *= i;
+  }
+  return result;
 };
 
 module.exports = {
