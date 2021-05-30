3cd980f19414e371a0084651c7048c0f836448b2
Rob Clarke <robclarke93@yahoo.co.uk>
2021-05-30 12:41:11 -0700

    another draft of caesar

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,7 +1,9 @@
 const caesar = function(text, shift) {
-    let text = prompt("Please enter text: ");
-    let shift = prompt("Please enter shift: ") % 26;
     
+    if (shift > 26 || shift < -26) {
+        shift = shift % 26;
+    }
+
     let newString = [];
 
     for (let i = 0; i < (text.length);i++) {
@@ -11,18 +13,38 @@ const caesar = function(text, shift) {
         //if capital
         if (number >= 65 && number <= 90) {
             //get shifted char code
-            if (shift > (90 - number)) {
-                newString[i] = String.fromCharCode(65 + (shift - (90 - number)));
-            } else {
-                newString[i] = String.fromCharCode(number + shift);
+            if (shift < 0) { // if negative
+                if ((number + shift) < 65) {
+                    newString[i] = String.fromCharCode(90 + (shift + 1));
+                } else {
+                    newString[i] = String.fromCharCode(shift + number);
+                }
+            } else { // if not negative
+                if (shift > (90 - number)) {
+                    newString[i] = String.fromCharCode(65 + (shift - (90 - number)));
+                } else {
+                    newString[i] = String.fromCharCode(number + shift);
+                } // end    
             } // end
+            
+
+
         //if lowercase    
         } else if (number >= 97 && number <= 122) {
-            if (shift > (122 - number)) {
-                newString[i] = String.fromCharCode(97 + (shift - (122 - number)));
+            if (shift < 0) { // if negative
+                if ((number + shift) < 97) {
+                    newString[i] = String.fromCharCode(122 + (shift + 1));
+                } else {
+                    newString[i] = String.fromCharCode(number + shift);
+                }
             } else {
-                newString[i] = String.fromCharCode(number + shift);
-            } // end
+                if (shift > (122 - number)) {
+                    newString[i] = String.fromCharCode(97 + (shift - (122 - number)));
+                } else {
+                    newString[i] = String.fromCharCode(number + shift);
+                } // end
+            }
+        // if not an alpha char - do nothing.  
         } else {
             newString[i] = text[i];
         } // end
@@ -33,10 +55,11 @@ const caesar = function(text, shift) {
     for (i = 0; i < newString.length; i++) {
          output += newString[i];
     }
+    console.log(output);
     return output;
     
 }
 
 caesar(text, shift);
 
-module.exports = caesar;
+//module.exports = caesar;
